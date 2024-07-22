e7af47ae150ed567f2e6c33b2d6457316209a2fd
legless <olegbeznogy@gmail.com>
2024-07-22 17:39:37 +0300

    3

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 const fileLink = document.createElement("a");
                 fileLink.href = file.download_url;
                 fileLink.innerText = file.name;
+                fileLink.style.color = getRandomColor();
 
                 fileIcon.src = pngIconUrl;
                 fileIcon.onerror = () => {
@@ -32,8 +33,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 fileItem.appendChild(fileIcon);
                 fileItem.appendChild(fileLink);
+                fileItem.addEventListener('click', () => {
+                    window.location.href = file.download_url;
+                });
+
                 fileListElement.appendChild(fileItem);
             });
         })
         .catch(error => console.error('Error fetching files:', error));
 });
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
